fd214d71377d7633fc1423b3912f54ef7e80e3f7
Andre Oliveira <dezin-oliveira@hotmail.com>
2024-07-17 21:13:53 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZedERP.Models.Entities;
+
+namespace ZedERP.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZedERP.Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(14)]
+        public required string Codigo { get; set; }
+        [Required]
+        [StringLength(255)]
+        public required string Name { get; set; }
+    }
+}
